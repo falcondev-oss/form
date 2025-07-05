81e7b0f6acb64b8b518b0ba38ab76b1cf2b236e2
Julian Meinking <12785972+DrJume@users.noreply.github.com>
2025-07-05 14:41:06 +0200

    chore(tsconfig): use base config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,11 +3,6 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   outDir: 'dist/',
   format: ['esm', 'cjs'],
-  dts: {
-    compilerOptions: {
-      composite: false,
-    },
-  },
   platform: 'browser',
   noExternal: ['on-change'],
   clean: true,
