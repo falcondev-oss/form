ea6c9cafe0c8fc831e3cf1a1c6367d586ca8ffd6
Julian Meinking <12785972+DrJume@users.noreply.github.com>
2025-07-05 14:46:28 +0200

    chore(tsdown): include dts in dist

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   outDir: 'dist/',
   format: ['esm', 'cjs'],
+  dts: {
+    compilerOptions: {
+      outDir: 'dist/',
+    },
+  },
   platform: 'browser',
   noExternal: ['on-change'],
   clean: true,
