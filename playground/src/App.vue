e3aaf590ca96daf0856cfeabf3b039f912d85ce0
Julian Meinking <12785972+DrJume@users.noreply.github.com>
2026-03-11 10:59:57 +0100

    build(deps): upgrade deps

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,43 +1,50 @@
 <script setup lang="ts">
 import { useForm } from '@falcondev-oss/form-vue'
+import { type } from 'arktype'
 import { reactive, watch } from 'vue'
 import z from 'zod'
 
-const data = reactive({
-  text: null as string | number | null,
-})
-watch(data, console.log)
-
 const form = useForm({
-  schema: z.object({
-    text: z
-      .string()
-      .min(5)
-      .max(20)
-      .nullable()
-      .meta({ title: 'Text' })
-      .or(z.number().min(0).max(100).meta({ title: 'Number' })),
+  schema: type({
+    date: type.Date.earlierThan(new Date(2025, 0, 1)),
   }),
-  sourceValues() {
-    console.log('sourceValues()')
-    return data
+  sourceValues: {
+    date: new Date(),
   },
   async submit({ values }) {
     console.log('Submitted', values)
-    await new Promise((r) => setTimeout(r, 1000))
-    data.text = `${values.text}!!`
   },
 })
 
-const textField = form.fields.text.$use()
+const textField = form.fields.date.$use({
+  translate: {
+    get(v) {
+      if (v instanceof Date) {
+        return v.toISOString().slice(0, 16)
+      } else if (typeof v === 'number') {
+        return new Date(v).toISOString().slice(0, 16)
+      }
+      return ''
+    },
+    set(v) {
+      const date = new Date(v)
+      if (!Number.isNaN(date.getTime())) {
+        return date
+      }
+      return null
+    },
+  },
+})
 </script>
 
 <template>
   <div class="flex flex-col">
     <pre>{{ form }}</pre>
-    <pre>{{ form.fields.text.$use().schema }}</pre>
-    <textarea
-      v-model.number="textField.model"
+    <pre>{{ form.fields.date.$use().schema }}</pre>
+    {{ textField.model }}
+    <input
+      v-model="textField.model"
+      type="datetime-local"
       class="h-32 w-full border p-2"
       :disabled="textField.disabled"
       @blur="textField.handleBlur"
